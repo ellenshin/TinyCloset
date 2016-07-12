f719ee92b7d1c3dcb75d68a081cc108bc9a65d7e
Ellen Shin <ellenshin1001@gmail.com>
2016-07-12 15:18:36 -0700

    Initial Commit

---
+++ What2Wear/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  What2Wear
+//
+//  Created by Ellen Shin on 7/12/16.
+//  Copyright © 2016 Ellen Shin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ What2Wear/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  What2Wear
+//
+//  Created by Ellen Shin on 7/12/16.
+//  Copyright © 2016 Ellen Shin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
